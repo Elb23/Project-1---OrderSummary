16be1333c970f7c2c6778d0d0993b49f6012c6ab
Elb23 <127051694+Elb23@users.noreply.github.com>
2023-07-14 13:28:58 +0100

    Add files via upload

---
+++ MultipDivision/Exercice1.js
@@ -0,0 +1,12 @@
+
+
+function multiply() {
+    a = document.getElementById("1stNumber").value;
+    b = document.getElementById("2ndNumber").value;
+    document.getElementById("result").innerHTML = a * b; 
+}
+
+function divide() {
+    a = document.getElementById("1stNumber").value;
+    b = document.getElementById("2ndNumber").value;
+    document.getElementById("result").innerHTML = a / b;    }
